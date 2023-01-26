871da63998b83a89c9ef0112cae9994ae3b97da0
SamueleRusco <119084990+SamueleRusco@users.noreply.github.com>
2023-01-26 15:19:38 +0100

    d

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 let tempo = document.getElementById("counter");
-let variabile = 0;
+let ultimaSessioneCounter = document.getElementById("counterGlobal");
+let variabile = localStorage.getItem("tempoGlobal");
+
 const bottone = document.querySelector("button");
 
 setInterval(timeIncrease, 1000);
 
 function timeIncrease() {
+  variabile = sessionStorage.getItem("tempo");
   variabile++;
+
   tempo.innerHTML = variabile;
+  localStorage.setItem("tempoGlobal", variabile);
+  sessionStorage.setItem("tempo", variabile);
 }
+
+window.onload = (event) => {
+  function counterSessione() {
+    ultimaSessioneCounter.innerHTML = getItem("tempoGlobal", variabile);
+  }
+};
